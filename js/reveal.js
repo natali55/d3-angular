f3d941a25728d8991c933041396968fa9a101c95
natali55 <natashamekh55@gmail.com>
2019-11-11 08:39:09 +0200

    pre final

--- js/reveal.js
+++ js/reveal.js
@@ -39,15 +39,15 @@
 
 			// The "normal" size of the presentation, aspect ratio will be preserved
 			// when the presentation is scaled to fit different resolutions
-			width: 960,
-			height: 700,
+			width: '100%',
+			height: '100%',
 
 			// Factor of the display size that should remain empty around the content
 			margin: 0.04,
 
 			// Bounds for smallest/largest possible scale to apply to content
-			minScale: 0.2,
-			maxScale: 2.0,
+			minScale: 1.0,
+			maxScale: 1.0,
 
 			// Display presentation control arrows
 			controls: true,
